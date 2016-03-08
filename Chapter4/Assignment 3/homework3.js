f31894021ec307b749c99a8e2a5828e939462c80
NeenuAVarghese <neenuavarghese@gmail.com>
2016-03-07 23:54:26 -0800

    Added Comments and changed call to main function

--- Chapter4/Assignment 3/homework3.js
+++ Chapter4/Assignment 3/homework3.js
@@ -4,6 +4,7 @@
 /*globals _*/
 "use strict";
 
+//Write a function that accepts an array of numbers as an argument and returns their average.
 function excercise1(array) {
     var totalsum = 0;
     array.forEach(function(value) {
@@ -11,7 +12,7 @@ function excercise1(array) {
     });
     return (totalsum / array.length);
 }
-
+//Write a function that accepts an array of numbers as an argument and returns the largest number in the array.
 function excercise2(array) {
     var max_val = array[0];
     array.forEach(function(value) {
@@ -21,7 +22,7 @@ function excercise2(array) {
     });
     return (max_val);
 }
-
+//Write a function that accepts an array of numbers and returns true if it contains at least one even number, false otherwise.
 function excercise3(array) {
     for (var val = 0; val < array.length; val++) {
         if (array[val] % 2 === 0) {
@@ -30,7 +31,7 @@ function excercise3(array) {
     }
     return false;
 }
-
+//Write a function that accepts an array of numbers and returns true if every number is even, false otherwise.
 function excercise4(array) {
     for (var val = 0; val < array.length; val++) {
         if (array[val] % 2 !== 0) {
@@ -40,7 +41,7 @@ function excercise4(array) {
     }
     return true;
 }
-
+//Write a function that accepts two arguments—an array of strings and a string—and returns true if the string is contained in the array, false otherwise.
 function arrayContains(strarray, word) {
     for (var val = 0; val < strarray.length; val++) {
         if (strarray[val].localeCompare(word) === 0) {
@@ -49,7 +50,7 @@ function arrayContains(strarray, word) {
     }
     return false;
 }
-
+//Write a function that is similar to the previous one, but returns true only if the array contains the given string at least twice
 function arrayContainsTwo(strarray, word) {
     var count = 0;
     for (var val = 0; val < strarray.length; val++) {
@@ -62,7 +63,7 @@ function arrayContainsTwo(strarray, word) {
     }
     return (false);
 }
-
+//Write a function that accepts three arguments and returns true if the array contains the element 3 times, where n is the third argument.
 function arrayContainsThree(strarray, word) {
     var count = 0;
     for (var val = 0; val < strarray.length; val++) {
@@ -75,7 +76,7 @@ function arrayContainsThree(strarray, word) {
     }
     return (false);
 }
-
+//Write a function that accepts three arguments and returns true if the array contains the element n times, where n is the third argument.
 function arrayContainsNTimes(strarray, word, n) {
     var count = 0;
     for (var val = 0; val < strarray.length; val++) {
@@ -88,19 +89,19 @@ function arrayContainsNTimes(strarray, word, n) {
     }
     return (false);
 }
-
+//Write a function that accepts an array of numbers as an argument and returns the largest number in the array.
 function excercise2underscore(array) {
     return _.max(array, function(num) {
         return num;
     });
 }
-
+//Write a function that accepts an array of numbers and returns true if it contains at least one even number, false otherwise.
 function excercise3underscore(array) {
     return _.some(array, function(num) {
         return num % 2 === 0;
     });
 }
-
+////Write a function that accepts an array of numbers and returns true if every number is even, false otherwise.
 function excercise4underscore(array) {
     return _.every(array, function(num) {
         return num % 2 === 0;
@@ -108,9 +109,6 @@ function excercise4underscore(array) {
 }
 
 var main = function() {
-    document.getElementById("excercise2underscore").innerHTML = excercise2underscore([3, 35, 43, 97, 47, 69, 8]);
-    document.getElementById("excercise3underscore").innerHTML = excercise3underscore([22, 32, 48, 54, 45, 77, 87]);
-    document.getElementById("excercise4underscore").innerHTML = excercise4underscore([2, 32, 42, 52, 62, 72, 82]);
     document.getElementById("excercise1").innerHTML = excercise1([42, 63, 42, 75, 26, 87, 98]);
     document.getElementById("excercise2").innerHTML = excercise2([3, 35, 43, 97, 47, 69, 8]);
     document.getElementById("excercise3").innerHTML = excercise3([45, 77, 87, 22, 32, 48, 54]);
@@ -119,6 +117,11 @@ var main = function() {
     document.getElementById("excercise6a").innerHTML = arrayContainsTwo(["hello", "world", "good", "morning", "web", "hello", "development", "hello", "morning", "Programming", "morning"], "hello");
     document.getElementById("excercise6b").innerHTML = arrayContainsThree(["hello", "world", "good", "morning", "web", "morning", "development", "morning", "morning", "Programming", "morning"], "morning");
     document.getElementById("excercise6c").innerHTML = arrayContainsNTimes(["web", "world", "good", "web", "web", "hello", "development", "hello", "web", "Programming", "morning"], "web", 4);
+    document.getElementById("excercise2underscore").innerHTML = excercise2underscore([3, 35, 43, 97, 47, 69, 8]);
+    document.getElementById("excercise3underscore").innerHTML = excercise3underscore([22, 32, 48, 54, 45, 77, 87]);
+    document.getElementById("excercise4underscore").innerHTML = excercise4underscore([2, 32, 42, 52, 62, 72, 82]);
 };
 
-$(document).ready(main);
+document.addEventListener("DOMContentLoaded", function() {
+    main();
+});
